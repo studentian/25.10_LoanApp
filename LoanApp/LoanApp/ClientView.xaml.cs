cdeb0a74bc2273de358a0ab8073a95aee326aae6
Ian Lee <L00150215@student.lyit.ie>
2018-10-26 12:20:45 +0100

    client view - xaml.cs renames to ClientView from MainWindow

--- LoanApp/LoanApp/ClientView.xaml.cs
+++ LoanApp/LoanApp/ClientView.xaml.cs
@@ -18,9 +18,9 @@ namespace LoanApp
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ClientView : Window
     {
-        public MainWindow()
+        public ClientView()
         {
             InitializeComponent();
         }
